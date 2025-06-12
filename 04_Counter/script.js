c145179fdbb1e3c785ee61ec9ba147e87364b0e5
Gaurav-rathi-12 <157203784+Gaurav-rathi-12@users.noreply.github.com>
2025-06-12 10:16:56 +0530

    This project is changes value of counter using increase and decrease.

---
+++ 04_Counter/script.js
@@ -0,0 +1,10 @@
+const counter = document.querySelector('.counter')
+const increase = document.querySelector('.increase')
+const decrease = document.querySelector('.decrease')
+
+increase.addEventListener('click',()=>{
+    counter.innerHTML++
+})
+decrease.addEventListener('click',()=>{
+    counter.innerHTML--
+})
